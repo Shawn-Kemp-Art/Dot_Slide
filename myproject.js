c2a5d2f642580ad70ee50cb5f584443d75adf12c
Shawn Kemp <shawn.kemp@gmail.com>
2023-10-28 10:27:01 -0700

    fixed frame scale

--- myproject.js
+++ myproject.js
@@ -102,7 +102,7 @@ var scale = 2;
 var ratio = 1/scale;//use 1/4 for 32x40 - 1/3 for 24x30 - 1/2 for 16x20 - 1/1 for 8x10
 
 var minOffset = ~~(7*ratio); //this is aproximatly .125"
-var framewidth = ~~(R.random_int(25, 50)*scale); 
+var framewidth = ~~(R.random_int(125, 200)*ratio); 
 //var framewidth = 50; 
     if (qfw){framewidth=qfw};
 
@@ -520,9 +520,6 @@ document.addEventListener('keypress', (event) => {
         //scale
        if(event.key == "1" || event.key =="2" ||event.key =="3" || event.key =="4") {
             setquery("scale",event.key);
-            setquery("w",5.6);
-            setquery("h",7);
-            setquery("cutmarks",1);
             location.reload();
             }
 
